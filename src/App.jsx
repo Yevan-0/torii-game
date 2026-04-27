ca4f925e697eb17d87ac2fcb5eaa9f541677c17a
Yevan-0 <yevand2006@gmail.com>
2026-04-28 00:42:11 +0530

    feat: changed background color

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { Suspense } from "react";
 function App() {
   return (
     <Canvas shadows camera={{ position: [0, 6, 14], fov: 42 }}>
-      <color attach="background" args={["#c9e7f0"]} />
+      <color attach="background" args={["#e3f6fc"]} />
       <fog attach="fog" args={["dbecfb", 30, 40]} />
       <Suspense>
         <Physics debug>
